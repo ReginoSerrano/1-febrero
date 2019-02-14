ee7f7ca99058b34e65f8048a411629ee2885d91a
ReginoSerrano <42398512+ReginoSerrano@users.noreply.github.com>
2019-02-13 21:07:44 -0600

    Create Pelota.js

---
+++ Pelota.js
@@ -0,0 +1,20 @@
+//Pelota
+var x = 25;
+var vel = 10;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  ellipse(x, 200, 50, 50);
+  x = x + vel;
+  if (x > 375) {
+    vel = vel*-1;
+  }
+  if (x < 25) {
+    vel=vel*-1;
+  }
+  console.log(x);
+}
